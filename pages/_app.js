9be778905c198685b30502e93bf4355e66aef864
lufyDev <vishalpundhirofficial@gmail.com>
2025-09-20 17:58:59 +0530

    added skills

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import StarsCanvas from "@/components/main/StarBackground";
+import NavBar from "@/components/main/NavBar";
 
 export default function App({ Component, pageProps }) {
   return (
     <div className="relative min-h-screen">
       <StarsCanvas />
+      <NavBar />
       <Component {...pageProps} />
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../components/main/Hero'
+import Skills from '@/components/main/Skills'
 
 const Home = () => {
   return (
     <div className='h-full w-full'>
       <div className='flex flex-col h-[850px] gap-20'>
         <Hero />
+        <Skills/>
       </div>
     </div>
   )
